1540d5fdd20951d68361d0e79c7b7e53b4c83a69
Ajmal Nabil <ajmalnabiljamal@gmail.com>
2018-06-01 12:19:29 +0800

    Add View pages and Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'links#index'
+  get "/:short_url", to: 'links#show'
+  get "shortened/:short_url", to: 'links#shortened', as: :shortened
+
+  post "/links/create"
+  get "/links/get_original_url"
 end
